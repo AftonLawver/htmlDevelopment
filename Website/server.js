da489b6709bf7dff9a1c45f99b509e77f11a192f
Afton Lawver <lawverap25@gmail.com>
2022-07-21 22:44:47 -0500

    Figured out that nodemon was restarting the server when the json file was written to. This was during another request/response cycle. Turned off nodemon and it worked great.

--- Website/server.js
+++ Website/server.js
@@ -47,6 +47,7 @@ app.post('/update', (req, res) => {
 
 app.post('/send', (req, res) => {
 
+    console.log('hello_1');
     let name = req.body['Name'];
     let email = req.body['Email'];
 
@@ -58,7 +59,7 @@ app.post('/send', (req, res) => {
         <p>Best,</p>
         <p>Afton Lawver</p>
     `
-
+    console.log('hello_2');
     const myOAuth2Client = new OAuth2 (
         process.env.OAUTH_CLIENTID,
         process.env.OAUTH_CLIENT_SECRET,
@@ -91,14 +92,16 @@ app.post('/send', (req, res) => {
         text: 'Dear ' + name + ',\n\nThanks for your feedback!',
         html: output
     };
-    s
+    console.log('hello_3');
     transporter.sendMail(mailOptions,function(err,result){
+        console.log('hello_4');
         if(err){
             console.log('Error with sending message.');
             res.send({
                 message:err
             })
         }else{
+            console.log('hello_5');
             transporter.close();
             console.log('Message sent successfully.');
 
@@ -106,6 +109,7 @@ app.post('/send', (req, res) => {
                 message:'Email has been sent: check your inbox!'
             })
         }
+        res.end();
     });
 });
 
